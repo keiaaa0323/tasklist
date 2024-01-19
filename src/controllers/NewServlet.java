a549105b887c5ee43e8a28f19c822d36f92d3135
keisu <keisuke1503230920@gmail.com>
2024-01-19 23:16:57 +0900

    Index/NewServletの修正

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -2,7 +2,7 @@ package controllers;
 
 import java.io.IOException;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
-import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
@@ -31,22 +30,14 @@ public class NewServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
-        em.getTransaction().begin();
+        // CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
 
-        Task t = new Task();
+        // おまじないとしてのインスタンスを生成
+        request.setAttribute("task", new Task());
 
-        String task = "task";
-        t.setTask(task);
-
-        // データベースに保存
-        em.persist(t);
-        em.getTransaction().commit();
-
-        response.getWriter().append(Integer.valueOf(t.getId()).toString());
-
-
-        em.close();
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+        rd.forward(request, response);
     }
 
 }
